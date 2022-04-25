a7618cbfcff04ff3d96e96093440a77b473a7775
adityadenny <adityadenny6@gmail.com>
2022-04-25 12:17:38 +0700

    finish create parallax scrolling with vanilla js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ window.addEventListener('scroll', function(){
     bird1.style.left = value * 2 + 'px';
     bird2.style.top = value * -1.5 + 'px';
     bird2.style.left = value * -5 + 'px';
-    btn.style.marginTop = value * -1.5 + 'px';
-    rocks.style.top = value * -0.12 + 'px';
-    
+    btn.style.marginTop = value * 1.5 + 'px';
+    rocks.style.top = value * -0.10 + 'px';
+    forest.style.top = value * 0.25 + 'px'; 
 })
